d5a3e9fa0429f118919c4fc8f656b3ea3e3f2f8b
猪股一歩希 <ibuki.bobby@gmail.com>
2025-01-25 17:52:34 +0900

    footer追加

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            Thank you to <a href="https://arxiv.org" target="_blank" rel="noopener noreferrer">arXiv</a> for use of its open access interoperability.
+        </footer>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@
 
 import { SessionProvider } from "next-auth/react"
 import "@/app/styles/global.css"
+import Footer from "@/app/components/Footer"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <SessionProvider>{children}</SessionProvider>
+        <Footer />
       </body>
     </html>
   )
